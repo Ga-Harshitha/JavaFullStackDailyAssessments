0b4731c517ddcf1acf280651bc61153313ddd537
Ga-Harshitha <163723158+Ga-Harshitha@users.noreply.github.com>
2024-06-14 20:18:08 +0530

    Delete Spring ORM/Configuring Spring ORM with Hibernate/LocalSessionFactoryBean.java

--- Spring ORM/Configuring Spring ORM with Hibernate/LocalSessionFactoryBean.java
+++
@@ -1,9 +0,0 @@
-public class EmployeeDao {
-    @Autowired
-    private SessionFactory sessionFactory;
-
-    @Transactional
-    public void saveEmployee(Employee employee) {
-        sessionFactory.getCurrentSession().save(employee);
-    }
-}
